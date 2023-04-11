054d19e6f70e006f66ad136e53a04de57468f90f
luciaragazzo <ragazzo.lucia@gmail.com>
2023-04-11 21:18:27 +0200

    Added search engine

--- src/app.js
+++ src/app.js
@@ -48,8 +48,21 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
+function search(city) {
 let apiKey = "b049602o5fdb8aa0eb6d9133t78bb72a";
-let city = "Roma, IT";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
+}
 
-https: axios.get(apiUrl).then(displayTemperature);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
